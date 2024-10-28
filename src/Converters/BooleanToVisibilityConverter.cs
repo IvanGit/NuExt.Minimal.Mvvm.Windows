7d54d3b4e5e1c660240379a755cc48eb3098939d
IvanGit <5630565+IvanGit@users.noreply.github.com>
2024-10-28 19:52:24 +0300

    Renaming files

---
+++ src/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿#if NETFRAMEWORK || WINDOWS
+using System.Globalization;
+using System.Windows;
+
+namespace Minimal.Mvvm.UI
+{
+    public sealed class BooleanToVisibilityConverter : ValueConverterBase<bool, Visibility, object?>
+    {
+        public bool HideInsteadOfCollapse { get; set; }
+
+        public bool Invert { get; set; }
+
+        protected override Visibility ConvertTo(bool value, object? parameter, CultureInfo? culture)
+        {
+            return (value ^ Invert) ? Visibility.Visible : HideInsteadOfCollapse ? Visibility.Hidden : Visibility.Collapsed;
+        }
+
+        protected override bool ConvertFrom(Visibility value, object? parameter, CultureInfo? culture)
+        {
+            return (value == Visibility.Visible) ^ Invert;
+        }
+    }
+}
+#endif
